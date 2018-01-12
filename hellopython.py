07d4a337f6358091b9913dc44b1168b060d21307
anksaini <theankushsaini@gmail.com>
2018-01-12 09:11:06 -0500

    py training I

---
+++ hellopython.py
@@ -0,0 +1,77 @@
+import random
+import sys
+import os
+'''
+print("Hello World")
+
+name = '"Ankush'
+lname = 'Saini Age - 27'
+
+newstring = name + lname
+print(newstring)
+
+print("%s %s %s" % ('My name is',name, lname))
+
+print("I don't like", end=" ")
+print("newlines")
+
+day = ['sun', 'mon', 'tue', 'wed']
+month = ['jan', 'feb', 'mar', 'apr']
+
+print(day[1:4])
+day_month = [day, month]
+
+print(day_month)
+print(day_month[0][2])
+
+month.append('may')
+print(month)
+
+day.insert(4, 'thu')
+print(day)
+
+day.remove('thu')
+print(day)
+
+day.sort()
+print(day)
+
+day.reverse()
+print(day)
+
+new = day + month
+
+print(new)
+print(min(new))
+
+oddn_tuple = (1,3,5,7,9)
+
+print(oddn_tuple)
+
+newl = list(oddn_tuple)
+print(newl)
+newt = tuple(newl)
+print(newt)
+
+print(max(newt)
+
+l_conve = {'Onion':'Pyaaz',
+         'Potato':'Aloo'}
+
+print(l_conve['Potato'])
+
+del l_conve['Onion']
+
+print(l_conve)
+print(l_conve.keys())
+print(l_conve.values())
+'''
+a = 9
+b = 6
+if a > b :
+    print('a is bada')
+elif a == b:
+    print('Ma Man')
+else:
+    print('b is bada')
+
