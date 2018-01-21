d486d8db8cb6f7725f8d7dd3f46ef1c2d6752a18
anksaini <anksaini27@gmail.com>
2018-01-21 13:03:02 -0500

    p47 examples

--- uselesstrivia.py
+++ uselesstrivia.py
@@ -1,7 +1,7 @@
 import random
 import sys
 import os
-
+'''
 name = input("What's your name?")
 age = int(input("How old are you?"))
 weight = int(input("Okay, last question. How many pounds do you weight?"))
@@ -14,3 +14,31 @@ print("You're over ",age*365*24*60*60,"seconds old")
 
 print("Did you know that on the moon you would weigh only ",weight/6,"pounds?")
 print("On the sun, you'd weigh ",weight*27.1,"<but, ah... not for long>")
+
+# p47.2
+
+food1 = input("Enter your fav food")
+food2 = input("Enter another fav food")
+
+print("Your fav food combo :",food1+food2)
+
+# p47.3
+
+billtotal = float(input("Enter the exact bill amount : "))
+bill15 = billtotal*0.15
+bill20 = billtotal*0.20
+print("Net bill with 15% tip : ", billtotal+bill15)
+print("Net bill with 20% tip : ", billtotal+bill20)
+
+
+#p47.4
+baseprice = float(input("Enter base price of the car : $"))
+tax = baseprice*0.20
+license = baseprice*0.10
+print("Base Price : $", baseprice)
+print("Tax : $", tax)
+print("License charge: $", license)
+print("Dealer Prep charge = $30")
+print("Destination charge = $70")
+print("Actual price of car : $", baseprice+tax+license+30+70)
+'''
