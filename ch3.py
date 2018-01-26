1452b6eb832b3ae3c33f9a3c848b9c7636ae028c
anksaini <anksaini27@gmail.com>
2018-01-26 14:52:03 -0500

    p85 examples

---
+++ ch3.py
@@ -0,0 +1,80 @@
+import random
+import sys
+import os
+
+# p51
+'''
+die1 = random.randint(1,6)
+die2 = random.randrange(6)+1
+
+print("You rolled a ",die1," and a ",die2," for a total of ",die1+die2)
+
+
+#p54
+password = input("Enter password : ")
+
+if password == "secret":
+    print("Access granted")
+    print("You important")
+else:
+    print("Access denied")
+    print("You ain't important")
+print("you always "
+      "get this")
+
+
+# p64
+response = ""
+
+while response != "cuz":
+    response = input("Why?\n")
+
+print("oh okay")
+
+i = 0
+while i<=10:
+    print(i)
+    i += 1
+print(i)
+
+
+
+count = 0
+
+while True:
+    count += 1
+    if count > 10:
+        break
+
+print(count)
+
+
+randnum = random.randint(1,100)
+picknum = int(input("Pick a number between 1 to 100\n"))
+i = 0
+
+while randnum != picknum:
+    if picknum > randnum:
+        picknum = int(input("Smaller!\n"))
+    else:
+        picknum = int(input("Larger!\n"))
+    i += 1
+
+print("Correct! You guessed it in ",i," tries")
+
+# p85/1
+
+randnum = random.randrange(1,6)
+print(randnum)
+if randnum == 1:
+    print("Sex")
+elif randnum == 2:
+    print("Money")
+elif randnum == 3:
+    print("Job")
+elif randnum == 4:
+    print("Peace")
+else:
+    print("Gold")
+'''
+# p85/2
